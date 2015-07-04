fccab02e6f77b98a923d2c18c3d8b3ca587541e3
Nicola Bombaci <sonybombaci@gmail.com>
2015-07-04 20:14:24 +0200

    Fix upload.php
    Target path

--- php/upload.php
+++ php/upload.php
@@ -5,10 +5,8 @@ $ds          = DIRECTORY_SEPARATOR;  //1
 if (!empty($_FILES)) {
      
     $tempFile = $_FILES['file']['tmp_name'];          //3             
-      
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
      
-    $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+    $targetFile =  $upload_url . $_FILES['file']['name'];  //5
  
     move_uploaded_file($tempFile,$targetFile); //6
      
